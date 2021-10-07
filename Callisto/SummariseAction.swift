afdf984f11db3057d317468637f2e62becc900b2
Patrick Kladek <patrick.kladek@outlook.com>
2021-10-07 17:54:47 +0400

    Update Callisto/SummariseAction.swift
    Co-authored-by: Michael Schwarz <michael_schwarz@me.com>

--- Callisto/SummariseAction.swift
+++ Callisto/SummariseAction.swift
@@ -24,7 +24,7 @@ final class Summarise: ParsableCommand {
     @Option(help: "Location for Output file", completion: .file(), transform: URL.init(fileURLWithPath:))
     var output: URL
 
-    @Option(help: "YML file to exclude messages from specific files", completion: .file(), transform: URL.init(fileURLWithPath:))
+    @Option(help: "YAML file to exclude messages from specific files", completion: .file(), transform: URL.init(fileURLWithPath:))
     var config: URL
 
     // MARK: - SummariseAction
