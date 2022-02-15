c1df0efa9666b5144200300f20a55589725357e1
Patrick Kladek <patrick.kladek@outlook.com>
2022-02-15 14:29:24 +0400

    fix copyright for FailBuildAction

--- Callisto/FailBuildAction.swift
+++ Callisto/FailBuildAction.swift
@@ -3,7 +3,7 @@
 //  Callisto
 //
 //  Created by Ammad on 03/02/2022.
-//  Copyright © 2022 IdeasOnCanvas. All rights reserved.
+//  Copyright © 2022 Bikemap. All rights reserved.
 //
 
 import Foundation
